e118c326d35fbf35c796ab78e1d02986ad8d9b91
2100031390 <110445212+2100031390@users.noreply.github.com>
2025-10-07 17:12:38 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://roombooking-backend-bmx9.onrender.com/api';
+const API_BASE_URL = 'https://roombooking-backend-cra5.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
